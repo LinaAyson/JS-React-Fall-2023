4d56ebab8f0b97434576d07015c611d89f1c278e
linaayson <andersson.lina89@gmail.com>
2023-10-10 15:42:44 +0200

    pokemon api exercise

--- weeks/week-3/exercises/exercise-1/fetch-pokemon-api.js
+++ weeks/week-3/exercises/exercise-1/fetch-pokemon-api.js
@@ -9,32 +9,53 @@
 // started, we've created some variables for you to use later on:
 
 const image = document.getElementById("image");
-console.log(image);
 const name = document.getElementById("name");
 const element = document.getElementById("element");
 const weight = document.getElementById("weight");
 const height = document.getElementById("height");
 const types = document.getElementById("types");
+// const strength = document.getElementById("strength");
 
 // 1) Start with updating the fetchPokemons function so that
 //    it's fetching the pokemons from the pokemon endpoint and
 //    logs the results in the console.
 //    HINT --> Don't forget to invoke the function
 
+const URL = "https://pokeapi.co/api/v2/pokemon/?limit=15";
+
 const fetchPokemons = async () => {
   /*Fetch all pokemons here*/
-
+  fetch(URL)
+    .then(response => response.json())
+    // .then(data => console.log(data.results))
+    .then(data => {
+      const pokemonNames = data.results.map(pokemon => pokemon.name);
+      console.log("NUMBER 1 " + pokemonNames);
+    })
+    .catch(error => {
+      //Handle error
+      console.error("NUMBER 1 " + "Error fetching pokemon data:", error)
+    })
 };
-//fetchPokemons();
+
+fetchPokemons();
 
 // 2) a) As you can see, we get some metadata as well as
 //    the results of the fetch. Change the console.log so
 //    that you only log the array of pokemon objects.
+//.then(data => console.log(data.results))
 
 //    b) Log only the name of the first pokemon in the
 //    pokemon objects array
+// .then(data => console.log(data.results[0]))
 
 //    c) Log the names of all pokemons in the array
+// .then(data => {
+//   const pokemonNames = data.results.map(pokemon => pokemon.name);
+//   console.log(pokemonNames);
+// })
+
+
 
 // 3) You might know that there are more than 20 pokemons
 //    in the pokedex (1050 to be exact). Add a query parameter
@@ -42,6 +63,8 @@ const fetchPokemons = async () => {
 //    choice, like this: https://pokeapi.co/api/v2/pokemon/?limit=151
 //    and pick a pokemon that you would like to continue
 //    working with. Copy the pokemon's URL.
+// "https://pokeapi.co/api/v2/pokemon/4/"
+
 
 // 4) Now that we've picked a pokemon, we will do a new fetch
 //    to the URL we copied. Since that's another endpoint,
@@ -49,9 +72,33 @@ const fetchPokemons = async () => {
 //    function (change the function's name to fit your pokemon).
 //    Log the data in the console and see what you find.
 
-const fetchCharizardData = async () => {
-  
+const urlcharamander = "https://pokeapi.co/api/v2/pokemon/4/";
+const sum = (total, x) => {
+  return total + x;
+}
+
+const fetchCharamanderData = async () => {
+  fetch(urlcharamander)
+    .then(response => response.json())
+    .then((data) => {
+      image.src = data.sprites.front_default;
+      name.textContent = data.name;
+      weight.textContent = data.weight;
+      height.textContent = data.height;
+      element.textContent = data.stats.map((stat) => stat.base_stat).reduce(sum, 0)
+      types.textContent = data.types.map((t) => t.type.name).toString();
+      console.log(data.name)
+    })
+    .catch(error => {
+      //Handle error
+      console.error("NUMBER 4 " + "Error fetching pokemon data:", error)
+    })
+
 };
+
+fetchCharamanderData();
+
+
 
 // 5) After familiarizing with the data, we will use the data
 //    to change our table. We will give you the image as a start.
@@ -60,6 +107,8 @@ const fetchCharizardData = async () => {
 //    image.src = json.sprites.front_default;
 //    Copy that line into the fetchBulbasaurData and hopefully
 //    the image in the HTML updates.
+
+
 
 // 6) Update the innerHTML of the other rows as well after
 //    you've found the correct path in the json.
@@ -70,3 +119,38 @@ const fetchCharizardData = async () => {
 // Check out the API's documentation and try to fetch from another
 // endpoint! There are many - as you can see in the first link
 
+const apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=15";
+
+function handleButtonClick() {
+  // Make a GET request to the API to fetch the list of Pokémon
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      // Choose a random Pokémon from the list (you can change the selection logic)
+      const randomPokemon = data.results[Math.floor(Math.random() * data.results.length)];
+
+      // Fetch data for the selected Pokémon
+      return fetch(randomPokemon.url);
+    })
+    .then(response => response.json())
+    .then(data => {
+      // Now you can access the data for the selected Pokémon
+      image.src = data.sprites.front_default;
+      name.textContent = data.name;
+      weight.textContent = data.weight;
+      height.textContent = data.height;
+      element.textContent = data.stats.map(stat => stat.base_stat).reduce((a, b) => a + b, 0);
+      types.textContent = data.types.map(t => t.type.name).toString();
+      console.log(data.name);
+    })
+    .catch(error => {
+      // Handle errors
+      console.error("Error fetching pokemon data:", error);
+    });
+}
+
+
+
+// Replace 'myButton' with the actual ID of your button
+const button = document.getElementById('myButton'); // Replace 'yourButtonId' with your button's actual ID
+button.addEventListener('click', handleButtonClick);
