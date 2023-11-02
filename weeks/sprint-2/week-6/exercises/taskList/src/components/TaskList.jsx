bc30774a3e68c8a6c445ace99a991a3ef33f2c54
linaayson <andersson.lina89@gmail.com>
2023-11-02 13:18:41 +0100

    tasklist fetching data

--- weeks/sprint-2/week-6/exercises/taskList/src/components/TaskList.jsx
+++ weeks/sprint-2/week-6/exercises/taskList/src/components/TaskList.jsx
@@ -1,5 +1,18 @@
-const TaskList = () => {
-  return <div>Task List</div>;
-};
+import "./TaskList.css";
 
-export default TaskList;
+export const TaskList = ({ tasks }) => {
+
+
+  return (
+    <div className="task-list">
+      {tasks &&
+        tasks.map((task) => (
+          <div key={task._id} className="cardTask">
+            <p>{task.description}</p>
+            <p>Date: {task.date}</p>
+            <p>Checked: {task.isChecked ? "Yes" : "No"}</p>
+          </div>
+        ))}
+    </div>
+  );
+};
